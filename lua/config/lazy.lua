d3a32ee9daff2030fa2c3b85ea32785686bd76fd
Sebastian Molina <smolinadunal.edu.co>
2024-07-23 00:12:23 -0500

    Update

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -9,5 +9,5 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
-	spec = { import = "config.plugins" } 
+  spec = { import = "config.plugins" }
 })
--- lua/config/plugins/mini-nvim.lua
+++ lua/config/plugins/mini-nvim.lua
@@ -4,6 +4,7 @@ return {
   config = function()
     require('mini.ai').setup { n_lines = 500 }
     require('mini.surround').setup()
+--    require('mini.icons').setup { style = 'ascii' }
     local statusline = require 'mini.statusline'
     statusline.setup { use_icons = vim.g.have_nerd_font }
     ---@diagnostic disable-next-line: duplicate-set-field
