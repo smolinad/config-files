861a20ba6868a087fcde2be15560abe684a30b87
Sebastian Molina <smolinadunal.edu.co>
2024-07-24 00:11:52 -0500

    Update

--- lua/config/plugins/mini-nvim.lua
+++ lua/config/plugins/mini-nvim.lua
@@ -4,9 +4,8 @@ return {
   config = function()
     require('mini.ai').setup { n_lines = 500 }
     require('mini.surround').setup()
-    require('mini.icons').setup { style = 'ascii' }
     local statusline = require('mini.statusline')
-    statusline.setup { use_icons = false }
+    statusline.setup { use_icons = vim.g.have_nerd_font }
     ---@diagnostic disable-next-line: duplicate-set-field
     statusline.section_location = function()
       return '%2l:%-2v'
