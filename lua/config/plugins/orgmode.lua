ee6782c60834232be6bc3d13240473c3e15e63d5
Sebastian Molina <smolinad@unal.edu.co>
2024-08-10 14:08:16 -0500

    Orgmode added

--- lua/config/plugins/orgmode.lua
+++ lua/config/plugins/orgmode.lua
@@ -4,9 +4,8 @@ return {
   event = 'VeryLazy',
   ft = { 'org' },
   config = function()
-    -- Setup orgmode
     require('orgmode').setup({
-      org_agenda_files = '~/orgfiles/**/*',
+      org_agenda_files = {'~/orgfiles/**/*', "G:\\My Drive\\orgfiles"},
       org_default_notes_file = '~/orgfiles/refile.org',
     })
   end
